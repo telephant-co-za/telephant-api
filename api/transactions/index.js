3c7e1a2c4e8451522dc7c0d4c2b8adc1d9aadc0d
warren-byron <warren.byron@gmail.com>
2021-06-05 06:57:10 +0200

    Including initial resources routes

--- api/transactions/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-//import { transactions, movieReviews, movieDetails } from './moviesData';
-
-const router = express.Router(); 
-router.get('/', (req, res) => {
-    res.json(movies);
-});
-
-export default router;
--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import transactionsRouter from './api/transactions';
+
+// import all resource routers
+import accountsRouter from './api/accounts';
+import contactsRouter from './api/contacts';
+import notificationsRouter from './api/notifcations';
+import retailersRouter from './api/retailers';
+import usersRouter from './api/users';
 
 dotenv.config();
-
 const app = express();
-
 const port = process.env.PORT;
 
-app.use('/api/transactions', transactionsRouter);
+app.use('/api/accounts', accountsRouter);
+app.use('/api/contacts', contactsRouter);
+app.use('/api/notifications', notificationsRouter);
+app.use('/api/retailers', retailersRouter);
+app.use('/api/users', usersRouter);
 
+// Run the server
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
