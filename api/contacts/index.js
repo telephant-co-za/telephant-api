948a69a4824568051a1db12091b6d1726ca80208
warren-byron <warren.byron@gmail.com>
2021-06-06 08:53:46 +0200

    Basic db lookup functionality working on GET /contacts

--- api/contacts/index.js
+++ api/contacts/index.js
@@ -1,16 +1,30 @@
 import express from 'express';
 import { NotExtended } from 'http-errors';
+import asyncHandler from 'express-async-handler';
+
+// Import models
+import contactModel from './contactModel';
 
 const router = express.Router();
 
 // GET return list of contacts
-router.get('/', (req, res, next) => {
-    const payload = {
-        'code': '200',
-        'message': 'cccc'
-    };
-    res.locals = payload;
-    next();
+router.get('/', async (req, res) => {
+
+    let { page = 1, limit = 20 } = req.query; // destructure page and limit and set default values
+    [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
+
+    const totalDocumentsPromise = contactModel.estimatedDocumentCount(); //Kick off async calls
+    const contactsPromise = contactModel.find().limit(limit).skip((page - 1) * limit);
+
+    const totalDocuments = await totalDocumentsPromise; //wait for the above promises to be fulfilled
+    const contacts = await contactsPromise;
+
+    const returnObject = { page: page, 
+                           total_pages: Math.ceil(totalDocuments / limit), 
+                           total_results: totalDocuments, 
+                           results: contacts };//construct return Object and insert into response object
+
+    res.status(200).json(returnObject);
 });
 
 // POST add a new contact
