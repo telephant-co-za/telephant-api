4d9c3222974aaeb0e158f1be64834617c340fa57
warren-byron <warren.byron@gmail.com>
2021-06-06 13:34:22 +0200

    Getting error handling working.

--- api/contacts/index.js
+++ api/contacts/index.js
@@ -1,45 +1,73 @@
 import express from 'express';
-import { NotExtended } from 'http-errors';
 import asyncHandler from 'express-async-handler';
+import { NotExtended } from 'http-errors';
 
 // Import models
 import contactModel from './contactModel';
 
 const router = express.Router();
+const createError = require('http-errors');
 
 // GET return list of contacts
-router.get('/', async (req, res) => {
+router
+    .get('/', asyncHandler(async (req, res) => {
 
-    let { page = 1, limit = 20 } = req.query; // destructure page and limit and set default values
-    [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
+        let { page = 1, limit = 20 } = req.query; // destructure page and limit and set default values
+        [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
+        try {
+            const totalDocumentsPromise = contactModel.estimatedDocumentCount(); //Kick off async calls
+            const contactsPromise = contactModel.find().limit(limit).skip((page - 1) * limit);
 
-    const totalDocumentsPromise = contactModel.estimatedDocumentCount(); //Kick off async calls
-    const contactsPromise = contactModel.find().limit(limit).skip((page - 1) * limit);
+            const totalDocuments = await totalDocumentsPromise; //wait for the above promises to be fulfilled
+            const contacts = await contactsPromise;
 
-    const totalDocuments = await totalDocumentsPromise; //wait for the above promises to be fulfilled
-    const contacts = await contactsPromise;
+            const returnObject = {
+                page: page,
+                total_pages: Math.ceil(totalDocuments / limit),
+                total_results: totalDocuments,
+                results: contacts
+            };//construct return Object and insert into response object
 
-    const returnObject = { page: page, 
-                           total_pages: Math.ceil(totalDocuments / limit), 
-                           total_results: totalDocuments, 
-                           results: contacts };//construct return Object and insert into response object
+            res.status(200).json(returnObject);
+        }
+        catch (error) {
+            createError(500, error);
+        }
+    }))
 
-    res.status(200).json(returnObject);
-});
+    // GET return details of a contact
+    .get('/:phoneq', asyncHandler(async (req, res, next) => {
+        try {
 
-// POST add a new contact
-router.post('/', (req, res) => {
-    res.json({"Items" : "add a new contact"});
-});
+            const phoneq = req.params.phoneq;
+            const contact = await contactModel.find({ phone: phoneq });
 
-// DELETE delete a specified contact
-router.delete('/', (req, res) => {
-    res.json({"will" : "delete a specified contact"});
-});
+            if (contact.length > 0) {
+                res.status(200).json(contact);
+            } else {
+                const err = createError(404, 'Could not find this phone number in your contacts.');
+                next(err);
+            }
+        }
+        catch (error) {
+            const err = createError(500, error);
+            next(err);
+        }
+    }))
 
-// PUT update a specified contact
-router.put('/', (req, res) => {
-    res.json({"will" : "update a specified contact"});
-});
+    // POST add a new contact
+    .get('/', asyncHandler(async (req, res) => {
+        res.json({ "Items": "add a new contact" });
+    }))
+
+    // DELETE delete a specified contact
+    .delete('/', (req, res) => {
+            res.json({ "will": "delete a specified contact" });
+    })
+
+    // PUT update a specified contact
+    .put('/', (req, res) => {
+            res.json({ "will": "update a specified contact" });
+    });
 
 export default router;
