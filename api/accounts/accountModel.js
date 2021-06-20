70375e3b00684fd693382915275b145abde535a6
warren-byron <warren.byron@gmail.com>
2021-06-20 11:51:45 +0200

    Working on account and balances.  Did some thinking about business process.

--- api/accounts/accountModel.js
+++ api/accounts/accountModel.js
@@ -3,8 +3,41 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const AccountSchema = new Schema({
-  username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  accountCode: { type: String, unique: true, required: true},
+  accountType: ['COA', 'GRP', 'USR'],
+  balance: {type: Number, required: true},
+  sign: ['+', '-']
+
 });
 
 export default mongoose.model('Account', AccountSchema);
+
+// ========================================================
+
+// Schema Decisions:
+
+// Note some aspects of the schema are not revealed to the public API and therefore not documented
+// in developer documentation
+
+// accountCode: must be of nature that fits in the bank reference field
+// This is so that automated scripts can pick the number from a bank deposits, credit card deposits,
+// cash deposits - this will happen via a web hook
+
+// accountType: 
+// COA: means chart of account - this is an internal account that is relevant to the business
+// The COA of accounts ensure that the allocation of funds is accurately represented
+// Also integration with a 3rd party accounting system will be possible later
+
+// GRP: means a group account
+// This type of account is for use by businesses, NPOs, charity orgs, governement etc
+// The unique id for these GRP accounts will be different to normal user accounts
+
+// USR: means a user account
+// This type of account is for use by a single user with a suitable south african mobile number
+
+// the balances in this collection with their relevant signs will balance to ZERO exactly
+// this is a trial balance - and will be used to identify issues on scheduled basis if not balanced
+
+// sign: is included because it indicates a DR or CR but depends on the perspective (view, form, report)
+// This will become relevant when writing auditing, verification and admin functionality
+
--- seedData/index.js
+++ seedData/index.js
@@ -1,15 +1,30 @@
 import contactModel from '../api/contacts/contactModel';
+import accountModel from '../api/accounts/accountModel';
 import { contacts } from './contacts';
+import { accounts } from './accounts';
 
 // deletes all user documents in collection and inserts test data
 export async function loadContacts() {
-  console.log('load contact Data');
+  console.log('Load Contacts collection data.');
   try {
 
     await contactModel.deleteMany();
     await contactModel.collection.insertMany(contacts);
     console.info(`${contacts.length} contacts were successfully stored.`);
   } catch (err) {
-    console.error(`failed to Load contacts Data: ${err}`);
+    console.error(`Failed to load Contacts data: ${err}`);
+  }
+}
+
+// deletes all account documents in collection and inserts test data
+export async function loadAccounts() {
+  console.log('Load Accounts collection data.');
+  try {
+
+    await accountModel.deleteMany();
+    await accountModel.collection.insertMany(accounts);
+    console.info(`${contacts.length} Aontacts were successfully stored.`);
+  } catch (err) {
+    console.error(`Failed to load Accounts data: ${err}`);
   }
 }
