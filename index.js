6c146cf7828fd7b2146b0942c7be13da69c108f1
warren-byron <warren.byron@gmail.com>
2021-06-26 09:22:04 +0200

    Added swagger document.  Doing planning in terms of the functionality and flow of the API. Added documentation folder for diagrams.

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ import airtimeRouter from './api/airtime/airtimeRouter';
 import contactsRouter from './api/contacts/contactsRouter';
 import notificationsRouter from './api/notifications';
 import retailersRouter from './api/retailers';
+import superRouter from './api/super/superRouter';
 import usersRouter from './api/users';
+
+// serve the swagger file
+// will change this later to be on seperate subdomain
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'js-yaml';
+import fs from 'fs';
+const swaggerDocument = yaml.load(fs.readFileSync('./swagger-docs/swagger.yml', 'utf8'));
 
 import wrongPath from './functions/wrongPath';
 const prepareErrors = require('./functions/prepareErrors');
@@ -34,7 +42,11 @@ app
 .use('/api/contacts', passport.authenticate('jwt', {session: false}), contactsRouter, prepareOutput)
 .use('/api/notifications', passport.authenticate('jwt', {session: false}), notificationsRouter, prepareOutput)
 .use('/api/retailers', passport.authenticate('jwt', {session: false}), retailersRouter, prepareOutput)
+.use('/api/super', superRouter, prepareOutput)
 .use('/api/users', usersRouter, prepareOutput)
+
+// / docs -> swagger docs
+.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // Catches all the wrong routes and refers person to documentation site
 .all('/*', wrongPath)
--- seedData/accounts.js
+++ seedData/accounts.js
@@ -4,9 +4,9 @@ export const accounts = [
     {"type":"USR", "name": "+27829524051", "balance": 250.00, "sign": true},
     {"type":"USR", "name": "+27829524061", "balance": 250.00, "sign": true},
     {"type":"USR", "name": "+27829524071", "balance": 250.00, "sign": true},
-    {"type":"USR", "name": "+27829524081", "balance": 250.00, "sign": true},
+    {"type":"USR", "name": "+27829524081", "balance": 250.00, "sign": false},
     {"type":"USR", "name": "+27829524091", "balance": 250.00, "sign": true},
     {"type":"USR", "name": "+27829524020", "balance": 250.00, "sign": true},
     {"type":"USR", "name": "+27829524011", "balance": 250.00, "sign": true},
-    {"type":"GRP", "name": "MyComapny", "balance": 2500.00, "sign": true}
+    {"type":"GRP", "name": "MyComapny", "balance": 2500.00, "sign": false}
 ];
