c2a1a4c406b9ed8ef00ea1ff0cfffa7b68e33874
warren-byron <warren.byron@gmail.com>
2021-06-27 16:25:47 +0200

    Working extensively on getting the swagger definition.

--- api/ping/pingRouter.js
+++ api/ping/pingRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express();
 
-router.all('/', (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).json({
         message: 'Pong.'
     });
